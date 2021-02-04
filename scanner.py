5a087c1868408f3aec00b75917eb03e9b437e390
vestitsky <40118064+vestitsky@users.noreply.github.com>
2021-02-04 19:54:36 +0300

    update scanner.py
    Fix comments

--- scanner.py
+++ scanner.py
@@ -5,46 +5,20 @@ import pytesseract
 from tkinter import *
 from tkinter import filedialog as fd
 
-'''Options for page segmentation modes(psm):
-
-0: Orientation and script detection (OSD) only.
-1: Automatic page segmentation with OSD.
-2: Automatic page segmentation, but no OSD, or OCR.
-3: Fully automatic page segmentation, but no OSD. (Default)
-4: Assume a single column of text of variable sizes.
-5: Assume a single uniform block of vertically aligned text.
-6: Assume a single uniform block of text.
-7: Treat the image as a single text line.
-8: Treat the image as a single word.
-9: Treat the image as a single word in a circle.
-10: Treat the image as a single character.
-11: Sparse text. Find as much text as possible in no particular order.
-12: Sparse text with OSD.
-13: Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
-
-OCR Engine Mode(oem)
-
-Tesseract has different engine modes for speed and performance
-
-0: Legacy engine only.
-1: Neural nets LSTM engine only.
-2: Legacy + LSTM engines.
-3: Default, based on what is available. '''
-
 root = Tk()
 
 root.title('Scanner')
-Label(root, text=os.getcwd()).pack() # Текущая директория
+Label(root, text=os.getcwd()).pack() # Local direcotory
 
 def scan_img():
-    # Отсканировать и добавить в буфер обмена
+    # Scan and add to clipboard
     root.clipboard_clear()
     label_final.delete('1.0',END)
     img = fd.askopenfilename()
     folder = img
     full_img = cv2.imread(img)
     custom_oem_psm_config = r'--oem 1 --psm 4'
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Admin\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Admin\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' # Local PATH of tesseract to scan image on Windows
     text = pytesseract.image_to_string(img,config=custom_oem_psm_config, lang="rus")
     label_filename.config(text=folder)
     label_final.insert(END,text)
